d5ec3c579e9a5cbc5bf43d6730db0ca7165d5fca
Ailton Junio <ailtonjunio97@gmail.com>
2023-08-16 20:27:42 -0300

    Implementação do Resultado

--- Jogo da Forca/src/aplicação/Programa.java
+++ Jogo da Forca/src/aplicação/Programa.java
@@ -6,14 +6,14 @@ import Entidade.Palavras;
 
 public class Programa {
 
-public static void main(String[] args) {
-		
+	public static void main(String[] args) {
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		Palavras pl = new Palavras();
-		
+
 		char[] c = null;
-		
+
 		System.out.println("-------------------------------");
 		System.out.println("-----------Bem Vindo-----------");
 		System.out.println("--------------ao---------------");
@@ -22,34 +22,36 @@ public static void main(String[] args) {
 		System.out.println();
 		System.out.println("Voçê tem 8 Tentativas!!");
 		String x = pl.getPalavras();
-		
+
 		c = x.toCharArray();
-		
+
 		System.out.println(x);
-		
+
 		int tamanho = x.length();
-		
+
 		// Teste de impressão de letras
-		/*for (int i = 0; i < x.length(); i++) {
-			System.out.print(c[i] + " ");
-		}*/
-		
-		for(int j = 0; j < tamanho; j++) {
-			System.out.print("_ ");
+		/*
+		 * for (int i = 0; i < x.length(); i++) { System.out.print(c[i] + " "); }
+		 */
+
+		String[] resultado = new String[tamanho];
+
+		for (int j = 0; j < tamanho; j++) {
+			resultado[j] = "_ ";
 		}
-		
-		for (int i = 0; i < 8; i++) {	
+
+		for (int i = 0; i < 8; i++) {
 			System.out.println();
 			System.out.println("Digite uma letra: ");
 			char letra = sc.next().charAt(0);
 			for (int j = 0; j < tamanho; j++) {
 				if (c[j] == letra) {
-					System.out.print(letra);
-				}	else {
-					System.out.print("_ ");
+					resultado[j] = letra + "";
 				}
 			}
+			System.out.println(Arrays.toString(resultado));
 		}
+
 		sc.close();
-	}	
+	}
 }
